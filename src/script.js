845b7ec77b2774492697c5de27690f1ea0b2336c
Jacky Madenfrost <jacky.madenfrost@gmail.com>
2021-07-14 10:56:42 +1000

    Fixed current time and date

--- src/script.js
+++ src/script.js
@@ -35,4 +35,34 @@ searchButton.addEventListener("click", citySearch);
 let h1 = document.querySelector("#city");
 h1.innerHTML = city;
 
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+
+h2.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}, ${year}`;
+
 axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
